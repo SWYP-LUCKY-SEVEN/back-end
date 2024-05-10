ef6c72020189ec243fca941b8f1d12406f754f9f
Jaeeun Park <58907538+park-jaeeun@users.noreply.github.com>
2024-05-10 16:07:35 +0900

    승인제 가입 신청 로직 구현

---
+++ src/main/java/com/example/swip/repository/JoinRequestRepository.java
@@ -0,0 +1,8 @@
+package com.example.swip.repository;
+
+import com.example.swip.entity.JoinRequest;
+import com.example.swip.entity.compositeKey.JoinRequestId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JoinRequestRepository extends JpaRepository<JoinRequest, JoinRequestId> {
+}
