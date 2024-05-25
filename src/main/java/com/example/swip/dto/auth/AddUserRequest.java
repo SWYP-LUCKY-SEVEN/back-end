c59da67c372e8ee87e1c90580270ec4716032c2a
Koak DoHun <dori20051@gmail.com>
2024-05-25 22:15:39 +0900

    hotfix: 사용자 임의 생성

--- src/main/java/com/example/swip/dto/auth/AddUserRequest.java
+++ src/main/java/com/example/swip/dto/auth/AddUserRequest.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @NoArgsConstructor
@@ -18,12 +20,15 @@ public class AddUserRequest {
         return User.builder()
                 .email(this.email)
                 .nickname(this.nickname)
+                .role("USER")
                 .build();
     }
     public User toTestEntity() {
         return User.builder()
+                .join_date(LocalDateTime.now())
                 .email(this.email)
                 .nickname(this.nickname)
+                .role("USER")
                 .validate("test")
                 .profile_image("http://res.cloudinary.com/dsfyp40dr/image/upload/v1714733931/p1zlj1jdedvvglagq9qw.png")
                 .build();
